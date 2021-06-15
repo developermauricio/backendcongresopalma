87c48b3b59ef77de7983e95a3a5c44cd88faea14
developermauricio <70330439+developermauricio@users.noreply.github.com>
2021-06-15 14:48:32 -0500

    sdsdsdsdsd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -47,7 +47,6 @@ class Controller extends BaseController
 
     public function setUserAuth(Request $request){
         $data = json_decode($request->users);
-        $this->getUsersAuth($data);
         return response()->json(['data' => $data]);
     }
 
