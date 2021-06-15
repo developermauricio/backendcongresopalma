7c0902e274b6204d9754c2ba62566f85d16d0fdc
developermauricio <70330439+developermauricio@users.noreply.github.com>
2021-06-15 15:14:25 -0500

    omoamsasas

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,11 +16,13 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function getData(Request $request){
+    public function getData(Request $request)
+    {
         return $request->param;
     }
 
-    public function setPoints(Request $request){
+    public function setPoints(Request $request)
+    {
         $success = true;
         $poinst = $request->points;
         $click = $request->idClick;
@@ -28,7 +30,7 @@ class Controller extends BaseController
         DB::beginTransaction();
         try {
             $setPoints = new Point;
-            $setPoints->points  = $poinst;
+            $setPoints->points = $poinst;
             $setPoints->user_id = 1;
             $setPoints->click_id = $click;
             $setPoints->save();
@@ -46,19 +48,32 @@ class Controller extends BaseController
 
     }
 
-    public function setUserAuth(Request $request){
+    public function setUserAuth(Request $request)
+    {
         /*$data = json_decode($request->param);*/
         $data = json_decode($request);
+        $userAuth = AuthUser::select('users_auth')->first();
+        $userAuth->users_auth = $request->param;
+        $userAuth->save();
+//        if ($userAuth) {
+//
+//            $authUser = new AuthUser;
+//            $authUser->users_auth = $request->param;
+//            $authUser->update();
+//        } else {
+//            $authUser = new AuthUser;
+//            $authUser->users_auth = $request->param;
+//            $authUser->save();
+//        }
 
-//        $authUser = new AuthUser;
-//        $authUser->users_auth = $request->param;
-//        $authUser->save();
 
         return response()->json(['data' => $data]);
     }
 
-    public function getUsersAuth(){
-        $data = AuthUser::select('users_auth')->get();
-        return response()->json(['data' => $data]);
+    public function getUsersAuth()
+    {
+        $data = AuthUser::select('users_auth')->first();
+        $jsoData = json_decode($data->users_auth);
+        return response()->json($jsoData);
     }
 }
