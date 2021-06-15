ddd7523a120c63099df21a12eaa7ae01253a4ad5
developermauricio <70330439+developermauricio@users.noreply.github.com>
2021-06-15 17:32:24 -0500

    oamsoamosas

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,8 +15,20 @@ class UserTableSeeder extends Seeder
 //        User::truncate();
 
         $user = new User;
-        $user->name = 'Mauricio';
-        $user->email = 'mao@gmail.com';
+        $user->name = 'admin';
+        $user->email = 'kevingonzalez.developer@gmail.com';
+        $user->password = 'comoquesi';
+        $user->save();
+
+        $user = new User;
+        $user->name = 'juan';
+        $user->email = 'tek16576@cuoly.com';
+        $user->password = 'comoquesi';
+        $user->save();
+
+        $user = new User;
+        $user->name = 'susan';
+        $user->email = 'susan@gmail.com';
         $user->password = 'comoquesi';
         $user->save();
 
